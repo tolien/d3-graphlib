b34a9116ba62c45670d1544c9d12c1f00c45f52c
tolien <tolien@users.noreply.github.com>
2015-08-21 11:00:34 +0000

    Polyfill for Math.sign

--- d3chart.js
+++ d3chart.js
@@ -1,3 +1,11 @@
+Math.sign = Math.sign || function(x) {
+    x = +x; // convert to a number
+    if (x === 0 || isNaN(x)) {
+        return x;
+    }
+    return x > 0 ? 1 : -1;
+}
+
 function chart(selection) {
   selection.each(function(data) {
   
